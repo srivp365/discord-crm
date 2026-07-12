510330aeb10e7cf1b186abd9d1f5c9e5681cbd60
SrivP <srivprasanna@gmail.com>
2026-07-12 21:02:32 +0530

    add note functionationality for users

--- discord-crm.py
+++ discord-crm.py
@@ -39,4 +39,34 @@ async def add_person(
     await ctx.respond(f"Post created successfully: {thread.mention}!", ephemeral=True)
 
 
+# A command to update/edit people
+@bot.slash_command(
+    name="add-note", description="Add a note to a person in your contacts"
+)
+async def add_note(
+    ctx: discord.ApplicationContext,
+    thread_id: discord.Option(discord.SlashCommandOptionType.string),
+    note: discord.Option(discord.SlashCommandOptionType.string),
+):
+    try:
+        # Convert the ID string to an integer
+        channel_id = int(thread_id)
+
+        # Fetch the forum post (threads are treated as channels)
+        thread = await bot.fetch_channel(channel_id)
+
+        # Verify the channel is actually a thread/forum post
+        if isinstance(thread, discord.Thread):
+            await thread.send(note)
+            await ctx.respond(
+                f"Successfully sent message to post: {thread.name}", ephemeral=True
+            )
+        else:
+            await ctx.respond(
+                "The provided ID is not a forum post/thread.", ephemeral=True
+            )
+    except:
+        await ctx.respond(f"Failed to add note to {thread.name}", ephemeral=True)
+
+
 bot.run(os.getenv("TOKEN"))  # run the bot with the token
