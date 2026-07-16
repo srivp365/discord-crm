f379a3333bd5911249a5491e7a8775607f9a9b19
srivp <srivdev365@gmail.com>
2026-07-16 14:35:09 +0530

    Update db.py to include the query to get birthdays for today

--- db.py
+++ db.py
@@ -2,16 +2,40 @@ import os
 
 import libsql
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+
 
 load_dotenv()  # load all the variables from the env file
+
+
+
+# establish a connection to db
 conn = libsql.connect(
     database=os.environ["TURSO_DATABASE_URL"],
     auth_token=os.environ["TURSO_AUTH_TOKEN"],
 )
 
-conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-conn.execute("INSERT INTO users (name) VALUES (?)", ("Alice",))
-conn.commit()
 
-rows = conn.execute("SELECT * FROM users").fetchall()
-print(rows)
+conn.execute("ALTER TABLE people ADD last_conected TEXT")
+
+
+def add_person_db(name, common_location, birthday):
+    conn.execute(
+        "INSERT INTO people (name, common_location, birthday) VALUES (?, ?, ?)",
+        (name, common_location, birthday),
+    )
+    conn.commit()
+
+def get_birthdays():
+    # grab today's date
+    today_utc = datetime.now(timezone.utc)
+    today_month_day = today_utc.strftime("%m-%d")
+
+    # grab birthdays
+    todays_birthdays = conn.execute(
+        "SELECT name FROM people WHERE strftime('%m-%d', birthday) = ?",
+        (today_month_day,)
+    ).fetchall()
+
+    print(f"These are the birthdays!: {todays_birthdays}")
+    return todays_birthdays
